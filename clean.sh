f83557a4e67326f5be8d9001d03de839ad31abe3
Egorov Roman <devabladablah@gmail.com>
2019-06-17 19:29:07 +0700

    added clean script

---
+++ clean.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+# todo: count how much space has been freed
+
+#read yn
+
+files[1]=()
+files+=(/home/user/.config/sublime-text-3/Cache/)
+files+=(/home/user/.config/VK/Cache/)
+files+=(/home/user/.gradle/caches/)
+files+=(/home/user/.gradle/daemon/)
+files+=(~/Desktop)
+
+for file in ~/Downloads/*
+do
+    file=${file%*/}      # remove the trailing "/"
+    #echo ${file##*/}    # print everything after the final "/"
+
+    if [ "${file##*/}" = "Telegram Desktop" ]; then
+        if [ "$(ls -A ~/Downloads/Telegram\ Desktop/)" ]; then
+            for telegramFile in ~/Downloads/Telegram\ Desktop/*
+            do
+                files+=(${telegramFile})
+            done
+        fi
+    else
+        files+=(${file})
+    fi
+done
+
+for file in ${files}
+do
+    if [ -d ${file} ]; then
+        echo "${file}"
+        rm -r ${file}
+    fi
+done
